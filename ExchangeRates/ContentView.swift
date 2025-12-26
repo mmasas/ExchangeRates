a8785306c58ab4a0a9438663f8fe2b0a1cb01b50
Moshe Masas <mmasas@gmail.com>
2025-12-26 20:13:39 +0200

    Initial Commit

---
+++ ExchangeRates/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExchangeRates
+//
+//  Created by Moshe Masas on 26/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExchangeRates/ExchangeRatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeRatesApp.swift
+//  ExchangeRates
+//
+//  Created by Moshe Masas on 26/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeRatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
